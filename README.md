aa6961f00351842294ee7a6b52e768c059d835eb
Bruno <bruno@eniac>
2019-02-13 22:14:49 -0200

    removed .py files

---
+++ README.md
@@ -0,0 +1,36 @@
+# Instalação
+Para executar a instalação automática basta copiar o conteúdo deste diretório para onde deseja instalar. A seguir, execute diretamente o script install.sh.
+
+```bash
+$ git clone https://github.com/bbanho/NeuroLib.git
+$ cd NeuroLib
+$ yes | sh install.sh
+```
+
+Ou apenas
+
+```bash
+$ git clone https://github.com/bbanho/NeuroLib.git; cd NeuroLib/auto; yes | sh install.sh
+```
+## Opcional
+Também é possível conceder permissão de executável.
+```bash
+$ chmod +x install.sh
+$ ./install.sh
+```
+## Etc
+```bash
+$ ./install.sh > log
+```
+O script se encarregará de realizar as instalações necessárias.
+
+Para testar se a instalação ocorreu corretamente, fornecer as imagens e executar
+```bash
+$ python3 NeuroLib.py
+$ python3 NeuroFrame.py
+```
+### TODO
+
+* Observar licença de software proprietário
+	* https://www.baslerweb.com/en/service/pylon-eula/
+
---
+++ install.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+INSTALLDIR=$PWD
+
+# add PPA reps
+sudo add-apt-repository ppa:nilarimogard/webupd8 --yes
+sudo add-apt-repository ppa:gijzelaar/snap7 --yes
+
+sudo apt update && sudo apt upgrade
+
+# Libs and other softwares
+sudo apt install -y  p7zip \
+		python3-pip \
+            	python3-setuptools \
+		python3-tk \
+	    	jupyter-notebook \
+            	git \
+		libsnap7-1 \
+		libsnap7-dev \
+		libgtk-3-dev \
+		libjpeg-dev \
+		libtiff-dev  \
+		libsdl1.2-dev \
+		libgstreamer1.0-dev \
+		libgstreamer-plugins-base1.0-dev \
+		libnotify-dev \
+		freeglut3 \
+		freeglut3-dev \
+		libsm-dev \
+		libwebkitgtk-dev \
+		libwebkitgtk-3.0-dev \
+
+
+# libsnap7
+
+# wget -q https://downloads.sourceforge.net/project/snap7/1.4.2/snap7-full-1.4.2.7z
+# 7z x snap7-full-1.4.2.7z
+# cd $INSTALLDIR/snap7-full-1.4.2/build/unix
+# make -f $INSTALLDIR/snap7-full-1.4.2/build/unix/x86_64_linux.mk
+# cd $INSTALLDIR
+
+# Pip packages
+pip3 install --user -r $INSTALLDIR/pip_packages.txt
+
+# Gooey
+pip3 install --user https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-16.04/wxPython-4.0.0b1-cp36-cp36m-linux_x86_64.whl
+git clone https://github.com/chriskiehl/Gooey.git
+cd Gooey
+# pip3 install --user -r $INSTALLDIR/Gooey/requirements.txt
+awk '!/wxpython/ {print}' $INSTALLDIR/Gooey/requirements.txt | pip3 install -r /dev/stdin
+sudo python3 $INSTALLDIR/Gooey/setup.py install
+# Workarround for Gooey libpng12
+wget http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb
+sudo dpkg -i libpng12-0_1.2.54-1ubuntu1_amd64.deb
+rm libpng12-0_1.2.54-1ubuntu1_amd64.deb
+cd $INSTALLDIR
+
+# Pylon installation
+# cd ~/pylon-5.1.0.12682-x86_64
+sudo tar -C /opt -xzf $INSTALLDIR/pylon-5.1.0.12682-x86_64/pylonSDK*.tar.gz
+sudo sh $INSTALLDIR/pylon-5.1.0.12682-x86_64/setup-usb.sh
+# cd /usr/bin
+sudo ln -s /opt/pylon5/bin/PylonViewerApp /usr/bin/PylonViewerApp
+
+
+# PyPylon
+wget https://github.com/basler/pypylon/releases/download/1.3.1/pypylon-1.3.1-cp36-cp36m-linux_x86_64.whl
+pip3 install --user pypylon-1.3.1-cp36-cp36m-linux_x86_64.whl
+rm pypylon-1.3.1-cp36-cp36m-linux_x86_64.whl -f
